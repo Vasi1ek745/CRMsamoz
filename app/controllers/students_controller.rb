0aa9982cd22415b1494caea55908d8399fb22a6b
Vasi1ek745 <vasi1ek745@gmail.com>
2022-06-21 15:32:51 +0500

    create method destroy and create in student contrller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,11 +11,27 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+    @student = Student.find(params[:id])
+    @student.delete
+    redirect_to students_path
+  end
+
+
+private
+  def student_params
+    params.require(:student).permit(:name , :subject, :class_in_school, :location, :pay_by_hour)  
+
   end
 end
